7da9c6e01765b32361e3b3095583358242c0877b
muramasa09 <70420722+muramasa09@users.noreply.github.com>
2020-09-16 20:01:47 +0900

    deviseのインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+=======
+  devise_for :installs
+  get 'messages/index'
+  root to: "messages#index"
+>>>>>>> Stashed changes
 end
